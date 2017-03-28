53a127fb15f8d478d90fba569c4ba70c7379bfc1
hrj <harshad.rj@gmail.com>
2017-03-28 17:13:28 +0530

    Script to create tiles

---
+++ misc/create-tiles.sh
@@ -0,0 +1,12 @@
+set -ex
+
+IMG=$1
+TILEX=$2
+TILEY=$3
+NAME=$4
+
+convert ${IMG} -crop ${TILEX}x${TILEY} \
+          -set filename:tile "%[fx:page.y/${TILEY}+1]-%[fx:page.x/${TILEX}+1]" \
+          +repage +adjoin "${NAME}-%[filename:tile].png"
+
+parallel flif {} {.}.flif ::: ${NAME}-*.png
