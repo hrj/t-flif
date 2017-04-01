2f3968854c2d821635ffc38e0856f9c3001ad8b6
hrj <harshad.rj@gmail.com>
2017-04-02 00:50:29 +0530

    emit output as png

--- src/main/scala/decoder.scala
+++ src/main/scala/decoder.scala
@@ -1,9 +1,13 @@
 package hrj.leaves
 
 import scala.sys.process._
+import java.awt.Rectangle
 
 case class TileOptions(name: String, width: Int, height: Int)
-case class DecodeOptions(top: Int, bottom:Int, left: Int, right: Int, downsample: Int)
+case class DecodeOptions(top: Int, bottom:Int, left: Int, right: Int, downsample: Int) {
+  def width = right - left + 1
+  def height = bottom - top + 1
+}
 
 case class DecodeResult()
 
@@ -14,17 +18,66 @@ object Decoder {
 
     val leftTile = (decodeOpts.left / tileOpts.width) + 1
     val rightTile = (decodeOpts.right / tileOpts.width) + 1
-    for (r <- topTile to bottomTile) {
-      for (c <- leftTile to rightTile) {
-        val fileName = s"${tileOpts.name}-$r-$c.flif"
-        val outFileName = s"out-$r-$c.png"
-        val command = s"flif -s ${decodeOpts.downsample} -d $fileName $outFileName"
-        println(command)
-        val result = command.!
-        println(result)
+
+    // TODO: result image type should be deduced / configurable
+    val resultImg = new java.awt.image.BufferedImage(decodeOpts.width, decodeOpts.height, java.awt.image.BufferedImage.TYPE_4BYTE_ABGR)
+    println(s"Decode wxh = ${decodeOpts.width} ${decodeOpts.height}")
+    val resultRaster = resultImg.getRaster
+
+    var dx = 0
+    var dy = 0
+
+    def mkSrcRect(r:Int, c:Int) = {
+      val srcX = if (c == leftTile) (decodeOpts.left % tileOpts.width) else 0
+      val srcY = if (r == topTile) (decodeOpts.top % tileOpts.height) else 0
+      val srcWidth = (if (c == rightTile) ((decodeOpts.right % tileOpts.width) + 1) else tileOpts.width) - srcX
+      val srcHeight = (if (r == bottomTile) ((decodeOpts.bottom % tileOpts.height) + 1) else tileOpts.height) - srcY 
+      val srcRect = new Rectangle(srcX, srcY, srcWidth, srcHeight)
+      srcRect
+    }
+
+    def readPng(pngFileName: String, r: Int, c: Int) = {
+      val srcRect = mkSrcRect(r, c)
+
+      val img = javax.imageio.ImageIO.read(new java.io.File(pngFileName))
+
+      val raster = img.getData
+      val data:Array[Int] = raster.getPixels(srcRect.x, srcRect.y, srcRect.width, srcRect.height, null)
+      // println("src rect: " + srcRect)
+      resultRaster.setPixels(dx, dy, srcRect.width, srcRect.height, data)
+      dx += srcRect.width
+      if (c == rightTile) {
+        dy += srcRect.height
+        dx = 0
       }
     }
 
+    for (r <- topTile to bottomTile) {
+      for (c <- leftTile to rightTile) {
+        flifDecode(tileOpts, decodeOpts, r, c) match {
+          case Some(pngFileName) => readPng(pngFileName, r, c)
+          case None => println(s"Unable to decode $r, $c")
+        }
+      }
+    }
+
+    javax.imageio.ImageIO.write(resultImg, "PNG", new java.io.File("result.png"))
+
     DecodeResult()
   }
+
+  private def flifDecode(tileOpts: TileOptions, decodeOpts: DecodeOptions, r: Int, c: Int) = {
+    val fileName = s"${tileOpts.name}-$r-$c.flif"
+    val outFileName = s"out-$r-$c.png"
+    // TODO: Avoid overwrite
+    val command = s"flif --overwrite -s ${decodeOpts.downsample} -d $fileName $outFileName"
+    // println(command)
+    val result = command.!
+    if (result == 0) {
+      Some(outFileName)
+    } else {
+      None
+    }
+  }
+
 }
