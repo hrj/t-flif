9a2073c8bf44686ba17247a58ce7fca39ac0217a
hrj <harshad.rj@gmail.com>
2017-04-03 13:54:49 +0530

    creation script: allow flif options to be specified

--- misc/create-tiles.sh
+++ misc/create-tiles.sh
@@ -1,12 +1,13 @@
 set -ex
 
 IMG=$1
-TILEX=$2
-TILEY=$3
+TILE_WIDTH=$2
+TILE_HEIGHT=$3
 NAME=$4
+FLIF_OPTS=$5
 
-convert ${IMG} -crop ${TILEX}x${TILEY} \
-          -set filename:tile "%[fx:page.y/${TILEY}+1]-%[fx:page.x/${TILEX}+1]" \
+convert ${IMG} -crop ${TILE_WIDTH}x${TILE_HEIGHT} \
+          -set filename:tile "%[fx:page.y/${TILE_HEIGHT}+1]-%[fx:page.x/${TILE_WIDTH}+1]" \
           +repage +adjoin "${NAME}-%[filename:tile].png"
 
-parallel flif -I {} {.}.flif ::: ${NAME}-*.png
+parallel flif ${FLIF_OPTS} -I {} {.}.flif ::: ${NAME}-*.png
